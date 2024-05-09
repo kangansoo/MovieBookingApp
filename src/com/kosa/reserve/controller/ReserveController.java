239d4a925bb700227db40e05f6178f0c393a2163
jaewoo9797 <9712jw@gmail.com>
2024-05-09 14:32:17 +0900

    스케쥴no프로시저추가

--- src/com/kosa/reserve/controller/ReserveController.java
+++ src/com/kosa/reserve/controller/ReserveController.java
@@ -3,5 +3,6 @@ package com.kosa.reserve.controller;
 import java.sql.SQLException;
 
 public interface ReserveController {
-	public int getReserveNo(int reserveQuantity, int memberNo) throws SQLException;
+	int getReserveNo(int reserveQuantity, int memberNo) throws SQLException;
+
 }
--- src/com/kosa/reserve/dao/ReserveDAO.java
+++ src/com/kosa/reserve/dao/ReserveDAO.java
@@ -6,6 +6,7 @@ import com.kosa.member.vo.MemberVO;
 
 public interface ReserveDAO {
 	public int insertReservation(int reserveQuantity, int memberVO) throws SQLException;
-	
-	
+
+	// 상영 DATE, TIME 상영관 이름으로 SCHEDULE_NO 가져오기
+	int getScheduleNo(String scheduleDate, String scheduleTime, String screenName) throws SQLException;
 }
